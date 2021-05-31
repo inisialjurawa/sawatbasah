b38921cf44ca83608c6a948d6987458a62329bbe
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-05-31 21:59:04 +0800

    Update takajutaran.js

--- takajutaran.js
+++ takajutaran.js
@@ -2,10 +2,10 @@ var urlmain = document.URL;
 var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
 
-"https://dev-neomoviefj.pantheonsite.io/visitourwebsite",
+"https://dev-neomoviefj.pantheonsite.io/",
 "https://dev-filmflixss.pantheonsite.io/",
-"https://dev-neoflimfj.pantheonsite.io/register",
-"https://dev-neofilmfj.pantheonsite.io/signup",
+"https://dev-neoflimfj.pantheonsite.io/",
+"https://dev-neofilmfj.pantheonsite.io/",
 "https://dev-filmfj.pantheonsite.io/"
 
 ];
