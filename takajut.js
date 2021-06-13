c62a35ebe94d377756730019ed59debf2792ceff
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-06-13 08:23:41 +0800

    Update takajut.js

--- takajut.js
+++ takajut.js
@@ -5,8 +5,8 @@ var dataUrl = [
   '//wonderfullytumultbizarre.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
   '//wonderfullytumultbizarre.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
   '//wonderfullytumultbizarre.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
-  '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
-  '//wonderfullytumultbizarre.com/n970yii7c?key=434c6176282b18468a76f961e6a21764'
+  '//wonderfullytumultbizarre.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
+  '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
