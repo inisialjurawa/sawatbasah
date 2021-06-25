a64ed394d93414131f7cd1fd5fd206b8a5a5946a
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-06-25 15:41:53 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -1,5 +1,9 @@
 var dataUrl = [
   '//wonderfullytumultbizarre.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+  '//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+  '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
+  '//wonderfullytumultbizarre.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
+  '//wonderfullytumultbizarre.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
   '//wonderfullytumultbizarre.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
