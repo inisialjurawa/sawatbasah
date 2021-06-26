08be8002eb35f7e5e39afbdde85c6b8ae564f244
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-06-26 08:32:05 +0800

    Update takajut.js

--- takajut.js
+++ takajut.js
@@ -3,6 +3,7 @@ var dataUrl = [
   '//wonderfullytumultbizarre.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
   '//wonderfullytumultbizarre.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
   '//wonderfullytumultbizarre.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+  '//dooloust.net/4/4251123',
   '//wonderfullytumultbizarre.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
   '//wonderfullytumultbizarre.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
   '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e'
