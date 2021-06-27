643b516d5c7060a34c601277438f9e34d3763a17
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-06-27 13:30:19 +0800

    Update cpat.js

--- cpat.js
+++ cpat.js
@@ -1,5 +1,7 @@
 var dataUrl = [
   '//bismuni.com/aagxk1htg?key=8766e82913b6419a1c60a7bee47c86c2',
+  '//thaudray.com/4/4246498',
+  '//ugroocuw.net/4/4252725',
   '//bismuni.com/fdk9pcwj?key=4879840d797239b77ace3ec695694e0f'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
