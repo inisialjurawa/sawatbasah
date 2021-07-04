68545260436b1be2cd72098ea8ac2c32d748c844
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-07-04 08:41:34 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -2,7 +2,7 @@ var dataUrl = [
   '//wonderfullytumultbizarre.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
   '//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
   '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
-  '//zuphaims.com/4/4252725',
+  '//wonderfullytumultbizarre.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
   '//wonderfullytumultbizarre.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
   '//wonderfullytumultbizarre.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
   '//wonderfullytumultbizarre.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851'
