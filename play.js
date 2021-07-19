e8ecf0bc34939fcd31574a2b4ab1f8082ab87295
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-07-19 23:44:54 +0800

    Update play.js

--- play.js
+++ play.js
@@ -3,8 +3,10 @@ var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
 "https://dev-filmfj.pantheonsite.io/",
 "https://dev-neomoviefj.pantheonsite.io/",
+"https://dev-neofilmfj.pantheonsite.io/",
 "https://dev-filmflixss.pantheonsite.io/",
-"https://dev-neoflimfj.pantheonsite.io/"
+"https://dev-neoflimfj.pantheonsite.io/",
+"https://dev-filmk32.pantheonsite.io/"
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem + param;
