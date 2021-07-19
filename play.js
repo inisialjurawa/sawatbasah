713769b28406e4f4ad502af8722b627486ec0695
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-07-19 23:33:04 +0800

    Update play.js

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 var urlmain = document.URL;
 var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
-"https://dev-carfilms1.pantheonsite.io/",
+"https://dev-filmfj.pantheonsite.io/",
 "https://dev-cusfilms1.pantheonsite.io/",
 "https://dev-neomoviefj.pantheonsite.io/",
 "https://dev-filmflixss.pantheonsite.io/",
