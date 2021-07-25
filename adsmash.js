dd2237daba8c16fb3728bdb7409464311879b7ec
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-07-26 02:48:14 +0800

    Update adsmash.js

--- adsmash.js
+++ adsmash.js
@@ -3,7 +3,7 @@ var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
 
  "https://bo.1st.yt/register.php?do=search&q=",
- "https://filmk21.blogspot.com/register+signup"
+ "https://filmk32.blogspot.com/"
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
