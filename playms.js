5d046f7c3f465b900b2cc02bae4d7955533be3c3
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-07-26 02:56:00 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -5,12 +5,12 @@ var dataUrl = [
 '//wonderfullytumultbizarre.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wonderfullytumultbizarre.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
 '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
- '//wonderfullytumultbizarre.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
+'//wonderfullytumultbizarre.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
 '//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
 '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
 '//wonderfullytumultbizarre.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
 '//wonderfullytumultbizarre.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'//neomoviefj2.blogspot.com/register_sign'
+'//filmfj.blogspot.com/'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
