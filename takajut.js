4ea1c92cd924c8a1e84d02edab1495bf67496fd0
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-07-28 15:33:03 +0800

    Update takajut.js

--- takajut.js
+++ takajut.js
@@ -1,5 +1,5 @@
 var dataUrl = [
-'//filmflixss.blogspot.com/',
+'//filmfj.blogspot.com/signup_',
 '//wonderfullytumultbizarre.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wonderfullytumultbizarre.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
 '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
