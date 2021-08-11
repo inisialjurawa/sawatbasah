43a66ad7a0d7e0cf7251afb6163397764a69e48f
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-08-11 15:34:12 +0800

    Update adsmash.js

--- adsmash.js
+++ adsmash.js
@@ -1,10 +1,10 @@
 var urlmain = document.URL;
 var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
-
- "https://bo.1st.yt/register.php?do=search&q=",
- "https://mybossfj.blogspot.com/signin_adstera="
-
+"https://bo.1st.yt/register.php?do=search&q=",
+"https://dev-neomoviefj.pantheonsite.io/",
+"https://dev-filmflixss.pantheonsite.io/"
+ 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem + param;
