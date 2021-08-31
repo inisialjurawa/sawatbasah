bfafba3349b01dc781931ec4387c8e0263227fb5
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-08-31 14:52:43 +0800

    Update takajutaran.js

--- takajutaran.js
+++ takajutaran.js
@@ -3,10 +3,11 @@ var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
 "https://dev-neomoviefj.pantheonsite.io/",
 "https://dev-filmflixss.pantheonsite.io/",
+"https://filmnewfj.blogspot.com/",
+"https://playfjs.blogspot.com/",
 "https://dev-neoflimfj.pantheonsite.io/",
 "https://dev-neofilmfj.pantheonsite.io/",
-"https://dev-filmfj.pantheonsite.io/",
-"https://playfjs.blogspot.com/"
+"https://dev-filmfj.pantheonsite.io/"
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem + param;
