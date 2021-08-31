29f67daf982e5b8899d64f12ed51d090d92b3f20
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-08-31 14:30:04 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -1,14 +1,13 @@
 var dataUrl = [
-'//wonderfullytumultbizarre.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
 '//wonderfullytumultbizarre.com/vxqhu00qt?key=8b7db070bef4e6586092ffbbf28a9def',
-'//wonderfullytumultbizarre.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
-'//wonderfullytumultbizarre.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
-'//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
-'//wonderfullytumultbizarre.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+'//wonderfullytumultbizarre.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'//wonderfullytumultbizarre.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
+'//wonderfullytumultbizarre.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 '//sergeantunhealthy.com/jdh7sy92?key=678ee6ec00a7dbcc74458748c5cbaa1f',
-'//wonderfullytumultbizarre.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
-'//wonderfullytumultbizarre.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a'
+'//capriceprop.com/p7wmzw69b6?key=8805a733dfbadc5f61cfab21de124b2a',
+'//capriceprop.com/dug7h0p8?key=aae9ffd149babf83a81f2c0eab30300f',
+'//capriceprop.com/jgh102dr2?key=294998ba132dc55926b6299c93ebf70f',
+'//capriceprop.com/jbjxyg1z?key=4de9306964f4da59cc607f8c3b7a1be0'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
