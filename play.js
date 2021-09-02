34d736f469f9ba19dd54192d8ba3df17297511c0
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-02 20:35:48 +0800

    Update play.js

--- play.js
+++ play.js
@@ -1,6 +1,7 @@
 var urlmain = document.URL;
 var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
+"https://dev-carfilms1.pantheonsite.io/",
 "https://filmnewfj.blogspot.com/",
 "https://dev-filmk32hd.pantheonsite.io/",
 "https://dev-filmfj.pantheonsite.io/",
@@ -10,7 +11,8 @@ var dataUrl = [
 "https://dev-neoflimfj.pantheonsite.io/",
 "https://dev-filmk32.pantheonsite.io/",
 "https://playfjs.blogspot.com/",
-"https://dev-filmk21.pantheonsite.io/"
+"https://dev-filmk21.pantheonsite.io/",
+"https://dev-cusfilms1.pantheonsite.io/"
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem + param;
