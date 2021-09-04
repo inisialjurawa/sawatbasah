ffc12aff618bf174df922bdfd4b4dd8a7f242035
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-04 11:30:52 +0800

    Update cpata.js

--- cpata.js
+++ cpata.js
@@ -3,8 +3,6 @@ var dataUrl = [
 '//goshbiopsy.com/dug7h0p8?key=aae9ffd149babf83a81f2c0eab30300f',
 '//goshbiopsy.com/jgh102dr2?key=294998ba132dc55926b6299c93ebf70f',
 '//goshbiopsy.com/jbjxyg1z?key=4de9306964f4da59cc607f8c3b7a1be0',
-'//thaudray.com/4/4246498',
-'//ugroocuw.net/4/4252725',
 '//sergeantunhealthy.com/jdh7sy92?key=678ee6ec00a7dbcc74458748c5cbaa1f'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
