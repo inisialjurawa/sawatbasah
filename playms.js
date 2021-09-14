b5c63f5432f95bb230391e5146cd613f47d50caf
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-14 18:38:53 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -3,7 +3,12 @@ var dataUrl = [
 '//wonderfullytumultbizarre.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
 '//wonderfullytumultbizarre.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
 '//wonderfullytumultbizarre.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
-'//wonderfullytumultbizarre.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
+'//wonderfullytumultbizarre.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'//goshbiopsy.com/p7wmzw69b6?key=8805a733dfbadc5f61cfab21de124b2a',
+'//goshbiopsy.com/dug7h0p8?key=aae9ffd149babf83a81f2c0eab30300f',
+'//goshbiopsy.com/jgh102dr2?key=294998ba132dc55926b6299c93ebf70f',
+'//goshbiopsy.com/jbjxyg1z?key=4de9306964f4da59cc607f8c3b7a1be0',
+'//goshbiopsy.com/yesucn64u?key=d3b2cba95843c8801b4168b6565e8b14'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
