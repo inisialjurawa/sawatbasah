e522e756a593c13b0fd01f590cd5aeda3979090f
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-18 07:37:13 +0800

    Update cpata.js

--- cpata.js
+++ cpata.js
@@ -1,9 +1,4 @@
 var dataUrl = [
-'//goshbiopsy.com/p7wmzw69b6?key=8805a733dfbadc5f61cfab21de124b2a',
-'//goshbiopsy.com/dug7h0p8?key=aae9ffd149babf83a81f2c0eab30300f',
-'//capriceprop.com/jgh102dr2?key=294998ba132dc55926b6299c93ebf70f',
-'//capriceprop.com/jbjxyg1z?key=4de9306964f4da59cc607f8c3b7a1be0',
-'//capriceprop.com/yesucn64u?key=d3b2cba95843c8801b4168b6565e8b14',
 '//wonderfullytumultbizarre.com/vxqhu00qt?key=8b7db070bef4e6586092ffbbf28a9def',
 '//wonderfullytumultbizarre.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
 '//wonderfullytumultbizarre.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
