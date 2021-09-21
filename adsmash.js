d13ccfb16cfb327a1b2431cc93241828135088db
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-21 08:07:18 +0800

    Update adsmash.js

--- adsmash.js
+++ adsmash.js
@@ -2,7 +2,7 @@ var urlmain = document.URL;
 var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
 "https://dev-neomoviefj.pantheonsite.io/",
-"https://bo.1st.yt/register.php?do=search&q=",
+"https://dev-filmfj.pantheonsite.io/",
 "https://dev-filmflixss.pantheonsite.io/"
  
 ];
