35af1a4534912e89c67dc00d4ad6ebff60b74e47
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-21 08:07:37 +0800

    Update takajutaran.js

--- takajutaran.js
+++ takajutaran.js
@@ -3,8 +3,7 @@ var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
 "https://dev-filmflixss.pantheonsite.io/",
 "https://dev-filmfj.pantheonsite.io/",	
-"https://dev-neomoviefj.pantheonsite.io/",	
-"https://filmnewfj.blogspot.com/",	
+"https://dev-neomoviefj.pantheonsite.io/",
 "https://dev-filmk32hd.pantheonsite.io/",
 "https://dev-neoflimfj.pantheonsite.io/",	
 "https://dev-neofilmfj.pantheonsite.io/"
