548f8ef594fbd0a5ab5794dfb660327984012ab5
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-09-23 12:04:56 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -7,7 +7,10 @@ var dataUrl = [
 '//wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
 '//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
 '//wavermerchandiseweird.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
-'//wavermerchandiseweird.com/n970yii7c?key=434c6176282b18468a76f961e6a21764'
+'//wavermerchandiseweird.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
+'//goshbiopsy.com/dug7h0p8?key=aae9ffd149babf83a81f2c0eab30300f',
+'//goshbiopsy.com/jgh102dr2?key=294998ba132dc55926b6299c93ebf70f',
+'//goshbiopsy.com/jbjxyg1z?key=4de9306964f4da59cc607f8c3b7a1be0'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
