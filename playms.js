df002d16669225fc803e7034966dd51f4bf254c8
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-10-04 16:18:06 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -1,18 +1,12 @@
 var dataUrl = [
+'https://wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 'https://wavermerchandiseweird.com/imzgcwsh?key=d0d188da2017ef9ee01fb562798d386a',
-'//wavermerchandiseweird.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
-'//wavermerchandiseweird.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
-'//wavermerchandiseweird.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
-'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'//wavermerchandiseweird.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
-'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
-'//wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
-'//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
-'//wavermerchandiseweird.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
-'//wavermerchandiseweird.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
-'//goshbiopsy.com/dug7h0p8?key=aae9ffd149babf83a81f2c0eab30300f',
-'//goshbiopsy.com/jgh102dr2?key=294998ba132dc55926b6299c93ebf70f',
-'//goshbiopsy.com/jbjxyg1z?key=4de9306964f4da59cc607f8c3b7a1be0'
+'https://wavermerchandiseweird.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
+'https://wavermerchandiseweird.com/vxqhu00qt?key=8b7db070bef4e6586092ffbbf28a9def',
+'https://wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'https://wavermerchandiseweird.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
+'https://wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'https://wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da817985'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
