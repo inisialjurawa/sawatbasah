3407d5ccb25b5b28edab526f3e3040b6af520994
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-10-07 19:59:32 +0800

    Create wafafra.js

---
+++ wafafra.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+'https://wavermerchandiseweird.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
+'https://wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'https://wavermerchandiseweird.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
+'https://wavermerchandiseweird.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
+'https://wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'https://wavermerchandiseweird.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
+'https://wavermerchandiseweird.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0'
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
