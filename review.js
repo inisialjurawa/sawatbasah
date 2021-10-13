4108bd6ad2b2a389a03936dde81164348038b000
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-10-13 08:17:16 +0800

    Update review.js

--- review.js
+++ review.js
@@ -2,6 +2,7 @@ var urlmain = document.URL;
 var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
 "https://dev-cusfilms1.pantheonsite.io/",
+"https://dev-filmflixss12.pantheonsite.io/",
 "https://dev-carfilms1.pantheonsite.io/"
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
