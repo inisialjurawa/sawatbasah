30c9b1f8e3808226e66c2a8ca4fe55cce0d49332
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-10-16 07:55:29 +0800

    Update playms.js

--- playms.js
+++ playms.js
@@ -1,10 +1,10 @@
 var dataUrl = [
-'https://wavermerchandiseweird.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
-'https://wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'https://wavermerchandiseweird.com/imzgcwsh?key=d0d188da2017ef9ee01fb562798d386a',
 'https://wavermerchandiseweird.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
+'https://wavermerchandiseweird.com/vxqhu00qt?key=8b7db070bef4e6586092ffbbf28a9def',
+'https://wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
 'https://wavermerchandiseweird.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
 'https://wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
-'https://wavermerchandiseweird.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
 'https://wavermerchandiseweird.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0'
   
 ];
