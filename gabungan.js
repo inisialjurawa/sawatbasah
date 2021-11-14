469eca6ea5a89acf768d7e0a80c07dc481beb02a
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-11-15 00:00:35 +0800

    Create gabungan.js

---
+++ gabungan.js
@@ -0,0 +1,22 @@
+var dataUrl = [
+'//wavermerchandiseweird.com/a8bdifyf?key=48e6c2ef69984c23a22ba95e4592b445',
+'//wavermerchandiseweird.com/imzgcwsh?key=d0d188da2017ef9ee01fb562798d386a',
+'//wavermerchandiseweird.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
+'//wavermerchandiseweird.com/vxqhu00qt?key=8b7db070bef4e6586092ffbbf28a9def',
+'https://wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'https://wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'https://wavermerchandiseweird.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+'https://wavermerchandiseweird.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+'https://wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
+'https://wavermerchandiseweird.com/mcq9dwrp?key=26877099e93b852a73d06043b22429bd',
+'https://wavermerchandiseweird.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e',
+'https://wavermerchandiseweird.com/n970yii7c?key=434c6176282b18468a76f961e6a21764',
+'https://wavermerchandiseweird.com/y425t3d5k?key=b0d195ca768aeab7d18aad598d71b0b0',
+'https://wavermerchandiseweird.com/nuudznpz3?key=3d998b3da5397f5ac865ff0734c2be19',
+'https://wavermerchandiseweird.com/uqf9166i4v?key=6863b7b7d27f7caf1b440a90c37c2ffb',
+'//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'//wavermerchandiseweird.com/ciwpm0hb7q?key=7f0434e3ed63cbba143ee2860b19fd5a',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
