455cb6fb37ea389c7b14127c79e7068dd65f9349
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-11-18 11:19:29 +0800

    Delete akunnew2.js

--- akunnew2.js
+++
@@ -1,6 +0,0 @@
-var dataUrl = [
-'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
