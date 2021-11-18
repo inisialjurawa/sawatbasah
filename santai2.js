d200e909841d8adb5ab116023c81897b8f125433
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-11-18 11:19:40 +0800

    Delete santai2.js

--- santai2.js
+++
@@ -1,7 +0,0 @@
-var urlmain = document.URL;
-var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
-var dataUrl = [
-"https://dev-filmflixss12.pantheonsite.io/"
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem + param;
